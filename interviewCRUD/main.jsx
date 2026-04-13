881dd75b32fc9c5a42b8555e0d57951b8902edf1
dipu01-code <dipendrakumarsenapati@gmail.com>
2026-04-13 16:02:38 +0530

    Delete interviewCRUD directory

--- interviewCRUD/main.jsx
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-import { createRoot } from 'react-dom/client';
-createRoot(document.getElementById('root')).render(
-    function App(){
-        return <h1>Interview CRUD</h1>
-        
-    }
-)
